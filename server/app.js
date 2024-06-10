b5b5f80f9f2ff7f6bd73d42fa43411484e0e4f51
Yves Martinez <yves.oliver.m@gmail.com>
2024-06-10 17:04:33 -0400

    get-cohorts complete

--- server/app.js
+++ server/app.js
@@ -6,11 +6,10 @@ const PORT = 5005;
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
-
+const cohortData = require("./cohorts.json");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
-
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
@@ -21,7 +20,6 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
@@ -29,6 +27,9 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+app.get("/api/cohorts", (req, res) => {
+  res.json(cohortData);
+});
 
 // START SERVER
 app.listen(PORT, () => {
