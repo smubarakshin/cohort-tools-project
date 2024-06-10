93b5f6d4cd075dac4f08b641bbab2efd38e59598
Yves Martinez <yves.oliver.m@gmail.com>
2024-06-10 17:42:33 -0400

    cors implemented

--- server/app.js
+++ server/app.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const PORT = 5005;
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
-const cohortData = require("./cohorts.json");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
-// ...
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
@@ -25,10 +30,6 @@ app.use(cookieParser());
 // ...
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
-});
-
-app.get("/api/cohorts", (req, res) => {
-  res.json(cohortData);
 });
 
 // START SERVER
